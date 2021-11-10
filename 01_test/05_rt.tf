aed88bf0727ea1eebd824c9fdc1d08410fb7d5ca
sonhyeji <kuy06046@gmail.com>
2021-11-10 17:29:27 +0900

    step1

---
+++ 01_test/05_rt.tf
@@ -0,0 +1,13 @@
+resource "aws_route_table" "hjson_rt" {
+  vpc_id = aws_vpc.hjson_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.hjson_ig.id
+
+  }
+  tags = {
+    "Name" = "hjson-rt"
+  }
+  
+}
